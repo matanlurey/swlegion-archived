9b4b803923df49d9c9bf7bd3d2fe6594d060c659
Matan Lurey <matan@lurey.org>
2018-12-12 12:40:36 -0800

    Initial tests.

--- lib/src/all_models.dart
+++ lib/src/all_models.dart
@@ -1,3 +1,9 @@
+import 'package:built_collection/built_collection.dart';
+import 'package:built_value/serializer.dart';
+
+// Import self in order to use in the metadata annotation.
+import 'all_models.dart';
+
 export 'model/attack_dice.dart' show AttackDice, AttackDiceSide;
 export 'model/attack_surge.dart' show AttackSurge;
 export 'model/defense_dice.dart' show DefenseDice, DefenseDiceSide;
@@ -8,3 +14,20 @@ export 'model/unit.dart' show Unit;
 export 'model/unit_type.dart' show UnitType;
 export 'model/upgrade_slot.dart' show UpgradeSlot;
 export 'model/weapon.dart' show Weapon;
+
+part 'all_models.g.dart';
+
+/// A [Serializers] for the models provided by this package.
+@SerializersFor([
+  AttackDice,
+  AttackSurge,
+  DefenseDice,
+  Faction,
+  Keyword,
+  Rank,
+  UnitType,
+  Unit,
+  UpgradeSlot,
+  Weapon,
+])
+final Serializers serializers = _$serializers;
---
+++ lib/src/all_models.g.dart
@@ -0,0 +1,52 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'all_models.dart';
+
+// **************************************************************************
+// BuiltValueGenerator
+// **************************************************************************
+
+// ignore_for_file: always_put_control_body_on_new_line
+// ignore_for_file: annotate_overrides
+// ignore_for_file: avoid_annotating_with_dynamic
+// ignore_for_file: avoid_catches_without_on_clauses
+// ignore_for_file: avoid_returning_this
+// ignore_for_file: lines_longer_than_80_chars
+// ignore_for_file: omit_local_variable_types
+// ignore_for_file: prefer_expression_function_bodies
+// ignore_for_file: sort_constructors_first
+// ignore_for_file: unnecessary_const
+// ignore_for_file: unnecessary_new
+// ignore_for_file: test_types_in_equals
+
+Serializers _$serializers = (new Serializers().toBuilder()
+      ..add(AttackDice.serializer)
+      ..add(AttackSurge.serializer)
+      ..add(DefenseDice.serializer)
+      ..add(Faction.serializer)
+      ..add(Keyword.serializer)
+      ..add(Rank.serializer)
+      ..add(Unit.serializer)
+      ..add(UnitType.serializer)
+      ..add(UpgradeSlot.serializer)
+      ..add(Weapon.serializer)
+      ..addBuilderFactory(
+          const FullType(BuiltMap,
+              const [const FullType(AttackDice), const FullType(int)]),
+          () => new MapBuilder<AttackDice, int>())
+      ..addBuilderFactory(
+          const FullType(BuiltMap,
+              const [const FullType(Keyword), const FullType(String)]),
+          () => new MapBuilder<Keyword, String>())
+      ..addBuilderFactory(
+          const FullType(BuiltMap,
+              const [const FullType(UpgradeSlot), const FullType(int)]),
+          () => new MapBuilder<UpgradeSlot, int>())
+      ..addBuilderFactory(
+          const FullType(BuiltSet, const [const FullType(Weapon)]),
+          () => new SetBuilder<Weapon>())
+      ..addBuilderFactory(
+          const FullType(BuiltMap,
+              const [const FullType(Keyword), const FullType(String)]),
+          () => new MapBuilder<Keyword, String>()))
+    .build();
