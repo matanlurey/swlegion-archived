cc2f9ff30558865cd5ccddb040ef378a8ac28260
Matan Lurey <matan@lurey.org>
2018-12-11 22:26:52 -0800

    Generated all_*.

---
+++ lib/src/all_units.dart
@@ -0,0 +1,47 @@
+import 'database/units/1.4_fd_laser_cannon_team.dart';
+import 'database/units/74_z_speeder_bike.dart';
+import 'database/units/at_rt.dart';
+import 'database/units/at_st.dart';
+import 'database/units/boba_fett.dart';
+import 'database/units/darth_vader.dart';
+import 'database/units/e_web_heavy_blaster_team.dart';
+import 'database/units/emperor_palpatine.dart';
+import 'database/units/fleet_troopers.dart';
+import 'database/units/general_veers.dart';
+import 'database/units/han_solo.dart';
+import 'database/units/imperial_royal_guards.dart';
+import 'database/units/leia_organa.dart';
+import 'database/units/luke_skywalker.dart';
+import 'database/units/rebel_commandos.dart';
+import 'database/units/rebel_commandos_strike_team.dart';
+import 'database/units/rebel_troopers.dart';
+import 'database/units/scout_troopers.dart';
+import 'database/units/scout_troopers_strike_team.dart';
+import 'database/units/snowtroopers.dart';
+import 'database/units/stormtroopers.dart';
+import 'database/units/t_47_airspeeder.dart';
+
+final aggregate = [
+  $1dot4FDLaserCannonTeam,
+  $74ZSpeederBikes,
+  atRt,
+  atSt,
+  bobaFett,
+  darthVader,
+  eWebHeavyBlasterTeam,
+  emperorPalpatine,
+  fleetTroopers,
+  generalVeers,
+  hanSolo,
+  imperialRoyalGuards,
+  leiaOrgana,
+  lukeSkywalker,
+  rebelCommandos,
+  rebelCommandosStrikeTeam,
+  rebelTroopers,
+  scoutTroopers,
+  scoutTroopersStrikeTeam,
+  snowtroopers,
+  stormtroopers,
+  t47Airspeeder
+];
