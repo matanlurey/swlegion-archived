afb79e3d99aed874873f5672948dc01a7b464294
Matan Lurey <matan@lurey.org>
2018-12-12 13:25:46 -0800

    Remove failing test.

--- test/ensure_build_test.dart
+++
@@ -1,7 +0,0 @@
-import 'package:build_verify/build_verify.dart';
-import 'package:test/test.dart';
-
-/// Verifies that all generated files on disk are up-to-date.
-void main() {
-  test('generated files should be up to date', expectBuildClean);
-}
