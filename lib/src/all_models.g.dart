babf02f26d67d2ca5c25e209a2d46375f7e9f983
Matan Lurey <matan@lurey.org>
2018-12-23 12:28:00 -0800

    Remove Army/ArmyUnit.

--- lib/src/all_models.g.dart
+++ lib/src/all_models.g.dart
@@ -7,8 +7,6 @@ part of 'all_models.dart';
 // **************************************************************************
 
 Serializers _$serializers = (new Serializers().toBuilder()
-      ..add(Army.serializer)
-      ..add(ArmyUnit.serializer)
       ..add(AttackDice.serializer)
       ..add(AttackSurge.serializer)
       ..add(CommandCard.serializer)
@@ -23,9 +21,6 @@ Serializers _$serializers = (new Serializers().toBuilder()
       ..add(UpgradeKey.serializer)
       ..add(UpgradeSlot.serializer)
       ..add(Weapon.serializer)
-      ..addBuilderFactory(
-          const FullType(BuiltList, const [const FullType(ArmyUnit)]),
-          () => new ListBuilder<ArmyUnit>())
       ..addBuilderFactory(
           const FullType(BuiltMap,
               const [const FullType(AttackDice), const FullType(int)]),
@@ -54,10 +49,7 @@ Serializers _$serializers = (new Serializers().toBuilder()
           () => new MapBuilder<Keyword, String>())
       ..addBuilderFactory(
           const FullType(BuiltSet, const [const FullType(Unit)]),
-          () => new SetBuilder<Unit>())
-      ..addBuilderFactory(
-          const FullType(BuiltSet, const [const FullType(UpgradeKey)]),
-          () => new SetBuilder<UpgradeKey>()))
+          () => new SetBuilder<Unit>()))
     .build();
 
 // ignore_for_file: always_put_control_body_on_new_line,annotate_overrides,avoid_annotating_with_dynamic,avoid_as,avoid_catches_without_on_clauses,avoid_returning_this,lines_longer_than_80_chars,omit_local_variable_types,prefer_expression_function_bodies,sort_constructors_first,test_types_in_equals,unnecessary_const,unnecessary_new
