f2dab69f830410ed6f9f3d28a069b07946ec9665
Matan Lurey <matan@lurey.org>
2019-02-08 16:01:58 -0800

    Finish adding assets with thumbnails.

--- tool/src/generate_thumbnails.dart
+++ tool/src/generate_thumbnails.dart
@@ -10,23 +10,52 @@ class _GenerateThumbnails extends Builder {
   @override
   Future<void> build(BuildStep buildStep) async {
     final bytes = await buildStep.readAsBytes(buildStep.inputId);
-    final image = _cropImage(
-      Image.fromBytes(0, 0, bytes),
-      buildStep.inputId.path,
-    );
+    final path = buildStep.inputId.path;
+    final image = decodePng(bytes);
     return buildStep.writeAsBytes(
-      buildStep.inputId.changeExtension('thumb.png'),
-      image.getBytes(),
+      buildStep.inputId.changeExtension('.thumb.png'),
+      encodePng(_cropImage(image, path)),
     );
   }
+
+  static final _commands = p.join(
+    'lib',
+    'assets',
+    'cards',
+    'commands',
+  );
+
+  static final _units = p.join(
+    'lib',
+    'assets',
+    'cards',
+    'units',
+  );
+
+  static final _upgrades = p.join(
+    'lib',
+    'assets',
+    'cards',
+    'upgrades',
+  );
 
   static Image _cropImage(Image input, String path) {
+    if (path.startsWith(_commands)) {
+      //  25 x  50
+      // 250 x 250
+      return copyCrop(input, 25, 50, 250, 250);
+    }
+    if (path.startsWith(_units)) {
+      // 270 x   0
+      //  94 x  50
+      return copyCrop(input, 270, 0, 94, 50);
+    }
+    if (path.startsWith(_upgrades)) {
+      //   0 x  30
+      // 200 x 110
+      return copyCrop(input, 0, 30, 200, 110);
+    }
     return input;
-  }
-
-  static String _fileName(String output) {
-    final file = p.withoutExtension(output);
-    return p.join('$file.thumb.png');
   }
 
   @override
